a0446034b24ec07db81c30da0c53af4fdc8992b9
unknown <butlermuwo@gmail.com>
2022-01-26 13:46:14 +0200

    Run tests to task.js files

---
+++ task1.test.js
@@ -0,0 +1,18 @@
+const stringLength = require("./task1");
+
+test('string length is 10', () => {
+    expect(stringLength('abcdefghij')).toBe(10);
+  });
+  test('string length is 3', () => {
+    expect(stringLength('abc')).toBe(3);
+  });
+  test('Too large', () => {
+    expect(() => {
+      stringLength('abcdefghijklm');
+    }).toThrow();
+  });
+  test('Too small', () => {
+    expect(() => {
+      stringLength('');
+    }).toThrow();
+  });
